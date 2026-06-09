94e127235fc307537fa198f576d9106736bb6403
Dennis Muoki <muokid3@gmail.com>
2026-06-09 19:21:48 +0300

    CMP Feature Convention Plugin

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -52,5 +52,14 @@ gradlePlugin {
             id = "com.berxley.convention.kmp.library"
             implementationClass = "KmpLibraryConventionPlugin"
         }
+
+        register("cmpLibrary") {
+            id = "com.berxley.convention.cmp.library"
+            implementationClass = "CmpLibraryConventionPlugin"
+        }
+        register("cmpFeature") {
+            id = "com.berxley.convention.cmp.feature"
+            implementationClass = "CmpFeatureConventionPlugin"
+        }
     }
 }
