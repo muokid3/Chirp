302a01868b7773cdf450834d0a24692ee806b396
Dennis Muoki <muokid3@gmail.com>
2026-06-10 16:52:19 +0300

    Theming setup

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -16,7 +16,7 @@ kotlin {
         commonMain {
             dependencies {
                 implementation(libs.kotlin.stdlib)
-                // Add KMP dependencies here
+                implementation(compose.components.resources)
             }
         }
 
