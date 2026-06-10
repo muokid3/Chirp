bb509ccc2cf44bb877ca27cb92a5be5f36372327
Dennis Muoki <muokid3@gmail.com>
2026-06-10 15:58:39 +0300

    String Resource Management

--- core/presentation/build.gradle.kts
+++ core/presentation/build.gradle.kts
@@ -16,6 +16,7 @@ kotlin {
 
                 // Add KMP dependencies here
                 implementation(projects.core.domain)
+                implementation(compose.components.resources)
 
             }
         }
