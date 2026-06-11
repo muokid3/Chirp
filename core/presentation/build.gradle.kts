2552e3d0b97e32b8583d47de3edd8c4e675fcc04
Dennis Muoki <muokid3@gmail.com>
2026-06-11 17:36:13 +0300

    device config class - screen size

--- core/presentation/build.gradle.kts
+++ core/presentation/build.gradle.kts
@@ -17,6 +17,7 @@ kotlin {
                 // Add KMP dependencies here
                 implementation(projects.core.domain)
                 implementation(compose.components.resources)
+                implementation(libs.material3.adaptive )
 
             }
         }
