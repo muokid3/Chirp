8f75eab2478df0c76ea21f70f217f8bef626879f
Dennis Muoki <muokid3@gmail.com>
2026-07-08 15:59:11 +0300

    Email Verification API call

--- core/data/src/commonMain/kotlin/com/berxley/core/data/auth/KtorAuthService.kt
+++ core/data/src/commonMain/kotlin/com/berxley/core/data/auth/KtorAuthService.kt
@@ -2,6 +2,7 @@ package com.berxley.core.data.auth
 
 import com.berxley.core.data.dto.requests.EmailRequest
 import com.berxley.core.data.dto.requests.RegisterRequest
+import com.berxley.core.data.networking.get
 import com.berxley.core.data.networking.post
 import com.berxley.core.domain.auth.AuthService
 import com.berxley.core.domain.util.DataError
@@ -31,6 +32,13 @@ class KtorAuthService(
         return httpClient.post(
             route = "/auth/resend-verification",
             body = EmailRequest(email)
+        )
+    }
+
+    override suspend fun verifyEmail(token: String): EmptyResult<DataError.Remote> {
+        return httpClient.get(
+            route = "/auth/verify",
+            queryParams = mapOf("token" to token)
         )
     }
 }
--- feature/auth/presentation/src/commonMain/kotlin/com/berxley/auth/presentation/email_verification/EmailVerificationViewModel.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/berxley/auth/presentation/email_verification/EmailVerificationViewModel.kt
@@ -1,27 +1,65 @@
 package com.berxley.auth.presentation.email_verification
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.berxley.core.domain.auth.AuthService
+import com.berxley.core.domain.util.onFailure
+import com.berxley.core.domain.util.onSuccess
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 
-class EmailVerificationViewModel : ViewModel() {
+class EmailVerificationViewModel(
+    private val authService: AuthService,
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    private var hasLoadedInitialData = false
+
+    private val token = savedStateHandle.get<String>("token")
 
     private val _state = MutableStateFlow(EmailVerificationState())
-    val state = _state.asStateFlow()
+    val state = _state
+        .onStart {
+            if (!hasLoadedInitialData) {
+                verifyEmail()
+                hasLoadedInitialData = true
+            }
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000L),
+            initialValue = EmailVerificationState()
+        )
 
     private val _channel = Channel<EmailVerificationEvent>()
     val channel = _channel.receiveAsFlow()
 
-    fun onAction(action: EmailVerificationAction) {
-        when (action) {
-            else -> TODO("Handle actions")
+    //Actions are purely for navigation
+    fun onAction(action: EmailVerificationAction) = Unit
+
+    private fun verifyEmail() {
+        viewModelScope.launch {
+            _state.update { it.copy(isVerifying = true) }
+
+            authService.verifyEmail(token ?: "Invalid token")
+                .onSuccess {
+
+                    _state.update { it.copy(isVerifying = false, isVerified = true) }
+
+                }
+                .onFailure { error ->
+                    _state.update { it.copy(isVerifying = false, isVerified = false) }
+
+                }
         }
     }
-
 }
